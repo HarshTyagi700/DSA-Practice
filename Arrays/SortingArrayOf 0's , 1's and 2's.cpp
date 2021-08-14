26c1d794630ba909767c2dfc90b3a7519598a5fa
Harsh Tyagi <48311361+HarshTyagi007@users.noreply.github.com>
2021-08-14 13:30:40 +0530

    Create SortingArrayOf 0's , 1's and 2's.cpp

---
+++ Arrays/SortingArrayOf 0's , 1's and 2's.cpp
@@ -0,0 +1,11 @@
+void sortColors(vector<int>& nums) {
+        int mid=0,low=0,high=nums.size()-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+                swap(nums[low++],nums[mid++]);
+            else if(nums[mid]==2)
+                swap(nums[mid],nums[high--]);
+            else mid++;  
+        }
+    }
