b4f06fe40a50ca3cea2894a6e7853d61fced0cfd
Harsh Tyagi <48311361+HarshTyagi007@users.noreply.github.com>
2021-10-15 19:37:25 +0530

    Create CoinChangeMinNumOfWays.cpp

---
+++ Dynamic Programming/CoinChangeMinNumOfWays.cpp
@@ -0,0 +1,65 @@
+/*
+
+https://leetcode.com/problems/coin-change/
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+ 
+
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+Example 4:
+
+Input: coins = [1], amount = 1
+Output: 1
+Example 5:
+
+Input: coins = [1], amount = 2
+Output: 2
+ 
+
+Constraints:
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+*/
+
+
+int coinChange(vector<int>& coins, int amount) {
+        int T[coins.size()+1][amount+1];
+        for(int i=0;i<=coins.size();i++)
+            T[i][0]=0;
+        for(int j=1;j<=amount;j++)
+        {
+            T[0][j]=INT_MAX-1;
+            if(j%coins[0]==0)//if completely divisible then put quotient
+                T[1][j]=j/coins[0];
+            else T[1][j]=INT_MAX-1; //otherwise put INT_MAX
+        }
+        for(int i=2;i<=coins.size();i++)
+            for(int j=1;j<=amount;j++)
+            {
+                if(coins[i-1]<=j)
+                    T[i][j]=min(T[i][j-coins[i-1]]+1,T[i-1][j]);
+                else T[i][j]=T[i-1][j];
+            }
+        if(T[coins.size()][amount]==INT_MAX-1)//if INT_MAX coins required that means it is not possible to generate that amount from the given coins
+            return -1;
+        else return T[coins.size()][amount];
+            
+    }
