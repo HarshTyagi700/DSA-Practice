feedbcb8e0de22701395c9366b03901f6df3e480
Harsh Tyagi <48311361+HarshTyagi007@users.noreply.github.com>
2022-07-06 09:52:55 +0530

    Histogram Optimized Solution Added

--- STACK/4. Maximum Area of Histogram.cpp
+++ STACK/4. Maximum Area of Histogram.cpp
@@ -68,3 +68,27 @@ vector<int> nearestSmallLeft(vector<int> &arr)
         }
         return ans;
     }
+
+    //Another Solution using single stack and single traversal
+    //TC- O(N) SC- O(N)
+    //here we'er calculating the area covered by heights[i] while popping it out from the stack
+    //while popping it out we know it is being popped bcz an element smaller than it is goin' to be 
+    //inserted so that will the it's rightSmaller
+    //and one below that popped element would be leftSmaller(would be -1 is stack is empty())
+    //that's how we can calculate the maxArea within one pass
+       int largestRectangleArea(vector<int>& heights) {
+       stack<int> st;//for storing idx in linear increasing fashion of ht
+        int maxArea=0,n=heights.size();
+        for(int i=0;i<=n;i++){
+                while(!st.empty() && 
+              (i==n ||heights[st.top()]>=heights[i])){
+                    int rightSmaller=i;
+                    int hT=heights[st.top()];
+                    st.pop();
+                    int leftSmaller=(!st.empty()?st.top():-1);
+                    maxArea=max(maxArea,hT*(rightSmaller-leftSmaller-1));
+                }           
+               st.push(i);               
+            }
+      return maxArea;
+    }
