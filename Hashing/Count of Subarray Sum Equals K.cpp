14e63ceb4c208b9f35cbd7146c9f38413772ebde
Harsh Tyagi <48311361+HarshTyagi007@users.noreply.github.com>
2022-07-11 19:30:13 +0530

    Hashing problem added

---
+++ Hashing/Count of Subarray Sum Equals K.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/subarray-sum-equals-k/
+int subarraySum(vector<int>& nums, int k) {
+         unordered_map<int,int> mp;//key->sum value->occurences
+        int j=0,i=0,sum=0,count=0;
+        mp[0]=1;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(mp.find(sum-k)!=mp.end())
+                count+=mp[sum-k];
+            mp[sum]++;
+        }
+        return count;
+    }
